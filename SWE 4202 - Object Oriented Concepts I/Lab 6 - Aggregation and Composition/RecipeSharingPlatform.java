c3a65068e8dd9e6fdec02514ab302aae3619c969
nafisreza <bigganinafis@gmail.com>
2024-03-01 02:46:24 +0600

    Lab 6 added

---
+++ SWE 4202 - Object Oriented Concepts I/Lab 6 - Aggregation and Composition/RecipeSharingPlatform.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Chef class
+class Chef {
+    private String chefName;
+    private List<Recipe> recipes;
+
+    public Chef(String chefName) {
+        this.chefName = chefName;
+        this.recipes = new ArrayList<>();
+    }
+
+    public void addRecipe(Recipe recipe) {
+        recipes.add(recipe);
+    }
+
+    public void removeRecipe(Recipe recipe) {
+        recipes.remove(recipe);
+    }
+
+    public String getChefName() {
+        return chefName;
+    }
+}
+
+// Recipe class
+class Recipe {
+    private String recipeName;
+    private List<Chef> chefs;
+
+    public Recipe(String recipeName) {
+        this.recipeName = recipeName;
+        this.chefs = new ArrayList<>();
+    }
+
+    public void addChef(Chef chef) {
+        chefs.add(chef);
+    }
+
+    public void removeChef(Chef chef) {
+        chefs.remove(chef);
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+}
+
+// User class
+class User {
+    private String userName;
+    List<Recipe> favoriteRecipes;
+    List<Chef> favoriteChefs;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.favoriteRecipes = new ArrayList<>();
+        this.favoriteChefs = new ArrayList<>();
+    }
+
+    public void addFavoriteRecipe(Recipe recipe) {
+        favoriteRecipes.add(recipe);
+    }
+
+    public void removeFavoriteRecipe(Recipe recipe) {
+        favoriteRecipes.remove(recipe);
+    }
+
+    public void addFavoriteChef(Chef chef) {
+        favoriteChefs.add(chef);
+    }
+
+    public void removeFavoriteChef(Chef chef) {
+        favoriteChefs.remove(chef);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
+
+public class RecipeSharingPlatform {
+    public static void main(String[] args) {
+        Chef chef1 = new Chef("Keka Ferdousi");
+        Chef chef2 = new Chef("Gordon Ramsey");
+
+        Recipe recipe1 = new Recipe("Kacchi Biriyani");
+        Recipe recipe2 = new Recipe("Pepperoni Pizza");
+
+        chef1.addRecipe(recipe1);
+        chef2.addRecipe(recipe2);
+
+        User user = new User("Alice");
+        user.addFavoriteRecipe(recipe1);
+        user.addFavoriteChef(chef2);
+
+        chef1.removeRecipe(recipe1); // This should not affect the recipe or user
+        chef2.removeRecipe(recipe2); // This should also remove the recipe from the platform
+
+        System.out.println("User: " + user.getUserName());
+        System.out.println("Favorite Recipes: " + user.favoriteRecipes);
+        System.out.println("Favorite Chefs: " + user.favoriteChefs);
+    }
+}
